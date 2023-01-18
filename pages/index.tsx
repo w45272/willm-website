7fa1693d7e879d5948450cafe72555212f15911d
w45272 <w45272@rams.colostate.edu>
2023-01-18 07:10:00 -0700

    yee

--- pages/index.tsx
+++ pages/index.tsx
@@ -35,8 +35,9 @@ function NavigationBar() {
             </svg>
           </button>
           <a href="#" className="flex">
-            <span className="sr-only">Logo</span>
-            <span className="inline-block h-10 w-32 rounded-lg bg-gray-200"></span>
+            {/* <span className="sr-only">Logo</span>
+            <span className="inline-block h-10 w-32 rounded-lg bg-gray-200"></span> */}
+            <h1>Will Mingus</h1>
           </a>
         </div>
         <div className="flex flex-1 items-center justify-end">
@@ -65,27 +66,21 @@ function NavigationBar() {
           <div className="ml-8 flex items-center">
             <div className="flex items-center divide-x divide-gray-100 border-x border-gray-100">
               <span>
-                <a href="/cart" className="block border-b-4 border-transparent p-6 hover:border-red-700">
-                  <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path clip-rule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" fill-rule="evenodd"></path>
-                  </svg>
-                  <span className="sr-only">Cart</span>
+                <a href="https://github.com/w45272" className="block border-b-4 border-transparent p-6 hover:border-red-700">
+                  <Image alt="GH"src="/github.svg" width="16" height="16"/>
+                  <span className="sr-only">GitHub</span>
                 </a>
               </span>
               <span>
-                <a href="/account" className="block border-b-4 border-transparent p-6 hover:border-red-700">
-                  <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path clip-rule="evenodd" fill-rule="even-odd" d="M28.778 1.004h-25.56c-0.008-0-0.017-0-0.027-0-1.199 0-2.172 0.964-2.186 2.159v25.672c0.014 1.196 0.987 2.161 2.186 2.161 0.010 0 0.019-0 0.029-0h25.555c0.008 0 0.018 0 0.028 0 1.2 0 2.175-0.963 2.194-2.159l0-0.002v-25.67c-0.019-1.197-0.994-2.161-2.195-2.161-0.010 0-0.019 0-0.029 0h0.001zM9.9 26.562h-4.454v-14.311h4.454zM7.674 10.293c-1.425 0-2.579-1.155-2.579-2.579s1.155-2.579 2.579-2.579c1.424 0 2.579 1.154 2.579 2.578v0c0 0.001 0 0.002 0 0.004 0 1.423-1.154 2.577-2.577 2.577-0.001 0-0.002 0-0.003 0h0zM26.556 26.562h-4.441v-6.959c0-1.66-0.034-3.795-2.314-3.795-2.316 0-2.669 1.806-2.669 3.673v7.082h-4.441v-14.311h4.266v1.951h0.058c0.828-1.395 2.326-2.315 4.039-2.315 0.061 0 0.121 0.001 0.181 0.003l-0.009-0c4.5 0 5.332 2.962 5.332 6.817v7.855z"></path>
-                  </svg>
-                  <span className="sr-only"> Account </span>
+                <a href="https://www.linkedin.com/in/will-mingus/" className="block border-b-4 border-transparent p-6 hover:border-red-700">
+                <Image alt="LI"src="/linkedin.svg" width="16" height="16"/>
+                  <span className="sr-only">LinkedIn</span>
                 </a>
               </span>
               <span className="hidden sm:block">
-                <a href="/search" className="block border-b-4 border-transparent p-6 hover:border-red-700" >
-                  <svg className="h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                  <span className="sr-only"> Search </span>
+                <a href="mailto:wdmingu@gmail.com" className="block border-b-4 border-transparent p-6 hover:border-red-700" >
+                <Image alt="LI"src="/email.svg" width="16" height="16"/>
+                  <span className="sr-only">Email</span>
                 </a>
               </span>
             </div>
