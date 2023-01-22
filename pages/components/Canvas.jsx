59bd42c87ecbe47c60d22371b456797853ba6206
w45272 <w45272@rams.colostate.edu>
2023-01-22 10:20:08 -0700

    Add Canvas

---
+++ pages/components/Canvas.jsx
@@ -0,0 +1,33 @@
+import React, { useRef, useEffect } from 'react'
+
+export default function Canvas(){
+  const canvasRef = useRef(null)
+  
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+    ctx.fillStyle = '#000FF0'
+    ctx.beginPath()
+    ctx.arc(50, 100, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI)
+    ctx.fill()
+  }
+  
+  useEffect(() => {
+    const canvas = canvasRef.current
+    const context = canvas.getContext('2d')
+    let frameCount = 0
+    let animationFrameId
+
+    function render(){
+      frameCount++
+      draw(context, frameCount)
+      animationFrameId = window.requestAnimationFrame(render)
+    }
+    render()
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId)
+    }
+  }, [draw])
+  
+  return <canvas ref={canvasRef}/>
+}
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,7 +1,10 @@
+import Canvas from "./components/Canvas";
 import NavigationBar from "./components/NavigationBar";
 
 export default function Projects() {
     return (<>
     <NavigationBar/>
+    <Canvas/>
+    <Button
     </>);
 }
