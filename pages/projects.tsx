6b5358fea22e118d2e4f2c542cf5facb2e6d4421
w45272 <w45272@rams.colostate.edu>
2023-01-22 10:41:14 -0700

    button group

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Canvas from "./components/Canvas";
 import NavigationBar from "./components/NavigationBar";
 
@@ -5,6 +6,18 @@ export default function Projects() {
     return (<>
     <NavigationBar/>
     <Canvas/>
-    <Button
+    
+<div className="inline-flex rounded-md shadow-sm" role="group">
+  <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+    Profile
+  </button>
+  <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+    Settings
+  </button>
+  <button type="button" className="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+    Messages
+  </button>
+</div>
+
     </>);
 }
